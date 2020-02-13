e60c4c0097f96b9acfcc201b4322d919cf124395
Hope Johnson <hopecaneel@gmail.com>
2020-02-12 23:25:47 -0500

    update bar chart with my data

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -1,22 +1,24 @@
-d3.csv("../../data/squirrelActivities.csv", d3.autoType).then(data => {
+var format = d3.format(",.0f");
+
+d3.csv("../../data/peds_agg.csv", d3.autoType).then(data => {
     console.log(data);
 
-const width = window.innerWidth * 0.9,
-    height = window.innerHeight / 3,
+const width = window.innerWidth * 0.8,
+    height = window.innerHeight / 2,
     paddingInner = 0.2;
-    margin = {top: 20, bottom: 40, left: 100, right: 40};
+    margin = {top: 20, bottom: 20, left: 60, right: 60};
 
   /** SCALES */
   // reference for d3.scales: https://github.com/d3/d3-scale
   const xScale = d3
     .scaleLinear()
-    .domain([0, d3.max(data, d => d.count)])
+    .domain([0, d3.max(data, d => d.avg_peds)])
     .range([margin.left, width - margin.right])
 
   const yScale = d3
     .scaleBand()
-    .domain(data.map(d => d.activity))
-    .range([height - margin.bottom, margin.top])
+    .domain(data.map(d => d.mon_year))
+    .range([margin.top, height - margin.bottom])
     .paddingInner(paddingInner);
 
   // reference for d3.axis: https://github.com/d3/d3-axis
@@ -29,15 +31,20 @@ const width = window.innerWidth * 0.9,
     .attr("width", width)
     .attr("height", height);
 
+  const title = svg.append("title");
+
+    title
+      .attr("class", "plot-title")
+      .text("Brooklyn Bridge Pedestrian Counts and Conditions");
+
   // append bars
   const rect = svg
     .selectAll("rect")
     .data(data)
     .join("rect")
-    .attr("y", d => yScale(d.activity))
+    .attr("y", d => yScale(d.mon_year))
     .attr("x", margin.left)
-    // .attr("width", d => width - margin.right - xScale(d.count))
-    .attr("width", d => xScale(d.count))
+    .attr("width", d => xScale(d.avg_peds))
     .attr("height", yScale.bandwidth())
     .attr("fill", "pink")
 
@@ -48,9 +55,9 @@ const width = window.innerWidth * 0.9,
     .join("text")
     .attr("class", "label")
     // this allows us to position the text in the center of the bar
-    .attr("x", d => xScale(d.count))
-    .attr("y", d => yScale(d.activity) + (yScale.bandwidth() / 2)) //dont really get this
-    .text(d => d.count)
+    .attr("x", d => xScale(d.avg_peds))
+    .attr("y", d => yScale(d.mon_year)) 
+    .text(d => format(d.avg_peds))
     .attr("dy", "1.25em");
 
   svg
