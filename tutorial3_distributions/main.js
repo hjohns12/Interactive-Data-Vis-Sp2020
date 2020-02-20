40abc943d10c5662d250ca6757816be203d461f4
Hope Johnson <hopecaneel@gmail.com>
2020-02-20 12:47:37 -0500

    add code for week 3

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -4,8 +4,6 @@ const width = window.innerWidth * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
   radius = 5;
 
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
-// All these variables are empty before we assign something to them.
 let svg;
 let xScale;
 let yScale;
@@ -13,12 +11,11 @@ let yScale;
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selection: "All" // + YOUR FILTER SELECTION
+  selectedDir: "Both",
 };
 
 /* LOAD DATA */
-d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
-  // + SET YOUR DATA PATH
+d3.csv("../data/peds_long.csv", d3.autoType).then(raw_data => {
   console.log("raw_data", raw_data);
   state.data = raw_data;
   init();
@@ -27,48 +24,109 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in 
 function init() {
-  // + SCALES
 
-  // + AXES
-
-  // + UI ELEMENT SETUP
+  xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(state.data, d => d.count))
+    .range([margin.left, width - margin.right]);
+  
+  yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(state.data, d => d.temperature))
+    .range([height - margin.bottom, margin.top]);
+  
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
 
   const selectElement = d3.select("#dropdown").on("change", function() {
-    // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
-
-    state.selection = this.value
     console.log("new value is", this.value);
+    state.selectedDir = this.value;
     draw(); // re-draw the graph based on this new selection
   });
 
-  // add in dropdown options from the unique values in the data
   selectElement
     .selectAll("option")
-    .data(["All", "1", "2", "3"]) // + ADD UNIQUE VALUES
+    .data(["Both", "Towards Manhattan", "Towards Brooklyn"]) 
     .join("option")
     .attr("value", d => d)
     .text(d => d);
 
   // + CREATE SVG ELEMENT
-
-  // + CALL AXES
-
-  draw(); // calls the draw function
+  svg = d3
+    .select('#d3-container')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+  
+  svg
+    .append("g")
+    .attr("class", "axis x-axis")
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
+    .call(xAxis)
+    .append("text")
+    .attr('class', 'axis-label')
+    .attr('x', '50%')
+    .attr('dy', '3em')
+    .text("Number of walkers (per day)");
+  
+  svg 
+    .append("g")
+    .attr('class', 'axis y-axis')
+    .attr('transform', `translate(${margin.left}, 0)`)
+    .call(yAxis)
+    .append('text')
+    .attr('class', 'axis-label')
+    .attr('y', '50%')
+    .attr('dx', '-3em')
+    .attr('writing-mode', 'vertical-rl')
+    .text("Temperature");
+  
+  draw();
 }
 
 /* DRAW FUNCTION */
  // we call this everytime there is an update to the data/state
 function draw() {
-  
-  // + FILTER DATA BASED ON STATE
-
-  // const dot = svg
-  //   .selectAll("circle")
-  //   .data(filteredData, d => d.name)
-  //   .join(
-  //     enter => enter, // + HANDLE ENTER SELECTION
-  //     update => update, // + HANDLE UPDATE SELECTION
-  //     exit => exit // + HANDLE EXIT SELECTION
-  //   );
+  let filteredData = state.data;
+  if (state.selectedDir !== 'Both') {
+    filteredData = state.data.filter(d => d.direction === state.selectedDir);
+    console.log(filteredData);
+  }
+  const dot = svg
+    .selectAll(".dot")
+    .data(filteredData, d => d.name)
+    .join(
+      enter =>
+        enter 
+        .append("circle")
+        .attr('class', 'dot')
+        .attr('stroke', 'green')
+        .attr('opacity', 0.4)
+        .attr('fill', d => {
+          if (d.direction === "Towards Manhattan") return "pink";
+          else return "purple";
+        })
+        .attr("r", radius)
+        .attr("cy", d => yScale(d.temperature))
+        .attr("cx", d => margin.bottom)
+        .call(enter => enter
+          .transition()
+          .attr("cx", d => xScale(d.count))
+        ),
+    update => 
+      update.call(update => 
+        update
+        .transition()
+        .duration(250)
+        .attr("stroke", "green")
+        ),
+    exit => 
+        exit.call(exit => 
+          exit
+          .transition()
+          .duration(10000)
+          .attr("cx", height)
+          .remove()
+          )
+    );
 }
