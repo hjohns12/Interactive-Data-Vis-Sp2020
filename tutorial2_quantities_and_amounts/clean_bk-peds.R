cea1d62d8c224fe5ba4714e65cc68e21c28f8d04
Hope Johnson <hopecaneel@gmail.com>
2020-02-20 08:06:20 -0500

    update cleaning script

--- tutorial2_quantities_and_amounts/clean_bk-peds.R
+++ tutorial2_quantities_and_amounts/clean_bk-peds.R
@@ -2,7 +2,11 @@ library(tidyverse)
 library(skimr)
 library(lubridate)
 
-dat <- read_csv(file.choose())
+dat <- read_csv("~/personal/Interactive-Data-Vis-Sp2020/data/Brooklyn_Bridge_Automated_Pedestrian_Counts_Demonstration_Project.csv")
+
+######
+###### For 2/13/20 horizontal barchart hw
+######
 dat$full_date <- mdy_hms(dat$hour_beginning)
 dat$day <- format(dat$full_date, "%m/%d/%Y")
 dat$mon_year <- format(dat$full_date, "%m/%Y")
@@ -24,3 +28,17 @@ dat_agg <- dat %>%
             n_obs = n())
 
 write.csv(dat_agg, "~/personal/Interactive-Data-Vis-Sp2020/data/peds_agg.csv")
+
+
+
+######
+###### For 2/20/20 scatterplot hw
+######
+dat <- read_csv("~/personal/Interactive-Data-Vis-Sp2020/data/Brooklyn_Bridge_Automated_Pedestrian_Counts_Demonstration_Project.csv")
+
+newdat <- dat %>%
+  gather(direction, count, `Towards Manhattan`:`Towards Brooklyn`) %>%
+  select(hour_beginning, temperature, direction, count)
+write.csv(newdat, "~/personal/Interactive-Data-Vis-Sp2020/data/peds_long.csv")
+
+
