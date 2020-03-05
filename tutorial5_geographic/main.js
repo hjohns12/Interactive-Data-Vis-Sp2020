070650aeb65652a8fc64cd5cb4162fd542f86b7b
Hope Johnson <hopecaneel@gmail.com>
2020-03-04 21:56:57 -0500

    add code for climate change map

--- tutorial5_geographic/main.js
+++ tutorial5_geographic/main.js
@@ -5,16 +5,20 @@ const width = window.innerWidth * 0.9,
   height = window.innerHeight * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 };
 
-/** these variables allow us to access anything we manipulate in
- * init() but need access to in draw().
- * All these variables are empty before we assign something to them.*/
 let svg;
 
 /**
  * APPLICATION STATE
  * */
 let state = {
-  // + SET UP STATE
+  geojson: null,
+  extremes: null,
+  hover: {
+    latitude: null,
+    longitude: null,
+    state: null,
+    changeDays: null,
+  },
 };
 
 /**
@@ -22,10 +26,11 @@ let state = {
  * Using a Promise.all([]), we can load more than one dataset at a time
  * */
 Promise.all([
-  d3.json("PATH_TO_YOUR_GEOJSON"),
-  d3.csv("PATH_TO_ANOTHER_DATASET", d3.autoType),
-]).then(([geojson, otherData]) => {
-  // + SET STATE WITH DATA
+  d3.json("../data/usState.json"),
+  d3.csv("../data/usHeatExtremes.csv", d3.autoType),
+]).then(([geojson, extremes]) => {
+  state.geojson = geojson;
+  state.extremes = extremes;
   console.log("state: ", state);
   init();
 });
@@ -36,17 +41,85 @@ Promise.all([
  * */
 function init() {
   // create an svg element in our main `d3-container` element
+  const projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
+  const path = d3.geoPath().projection(projection);
+
   svg = d3
     .select("#d3-container")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-  // + SET UP PROJECTION
-  // + SET UP GEOPATH
-
   // + DRAW BASE MAP PATH
+  svg
+    .selectAll(".state")
+    .data(state.geojson.features)
+    .join("path")
+    .attr("d", path)
+    .attr("class", "state")
+    .attr("fill", "transparent")
+    .on("mouseover", d => {
+      state.hover["state"] = d.properties.NAME;
+      draw();
+    });
+
+  color = d3.scaleQuantize()
+    .domain(d3.extent(state.extremes, d => d["Change in 95 percent Days"]))
+    .range(["lightblue", "white", "orange"])
+  
+  myRadius = d3.scaleQuantize()
+    .domain(d3.extent(state.extremes, d => d["Change in 95 percent Days"]))
+    .range([10, 2, 10])
+
+  const formatNumber = d3.format("d")
+
+  const points = svg
+    .selectAll("circle")
+    .data(state.extremes)
+    .join("circle")
+    .attr("r", d => myRadius(d["Change in 95 percent Days"]))
+    .attr("stroke", "black")
+    .attr("fill", d => color(d["Change in 95 percent Days"]))
+    .attr("cx", function(d) { 
+      return projection([d.Long,d.Lat])[0];})
+    .attr("cy",function(d) { 
+      return projection([d.Long,d.Lat])[1];})
+    .attr("changeDays", function(d) {
+      return d["Change in 95 percent Days"]})
+    .on("mouseover", d => {
+      state.hover["changeDays"] = formatNumber(d["Change in 95 percent Days"]);
+      draw();
+    })
+
   // + ADD EVENT LISTENERS (if you want)
+  svg.on("mousemove", () => {
+    // we can use d3.mouse() to tell us the exact x and y positions of our cursor
+    const [mx, my] = d3.mouse(svg.node());
+    // projection can be inverted to return [lat, long] from [x, y] in pixels
+    const proj = projection.invert([mx, my]);
+    state.hover["longitude"] = proj[0];
+    state.hover["latitude"] = proj[1];
+    draw();
+  });
+
+  // const onePoint = svg
+  //   .append("circle")
+  //   .attr("fill", "lightblue")
+  //   .attr("r", 10)
+  //   .attr("stroke", "black")
+  //   .attr("transform", "translate(11,20)")
+
+  // const twoPoint = svg
+  //   .append("circle")
+  //   .attr("fill", "orange")
+  //   .attr("r", 10)
+  //   .attr("stroke", "black")
+  //   .attr("transform", "translate(11,50)")
+  
+  // legendText = d3.select("#legend-container")
+  //   .append("text")
+  //   .text("   Orange points show where these unusually hot days are becoming more common.")
+
 
   draw(); // calls the draw function
 }
@@ -55,4 +128,18 @@ function init() {
  * DRAW FUNCTION
  * we call this everytime there is an update to the data/state
  * */
-function draw() {}
+function draw() {
+  hoverData = Object.entries(state.hover);
+  console.log(hoverData);
+
+  hoverContainer = d3.select("#hover-container")
+    .selectAll("div.row")
+    .data(hoverData)
+    .join("div")
+    .attr("class", "row")
+    .html(
+      d => 
+      d[1] ? `${d[0]}: ${d[1]}` : null
+    );
+  
+}
